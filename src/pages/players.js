21d756cb5cdace3377300d10d7fffddafe9b5eb2
Kevin Miller <keveemiller@gmail.com>
2020-05-18 20:58:17 -0400

    chore: Move player list to component

--- src/pages/players.js
+++ src/pages/players.js
@@ -1,40 +1,14 @@
 import React from 'react'
 import { Container } from '../components/common/container'
 import Layout from '../components/layout'
-import getPlayerGames from '../utilities/player-games'
-import { Link, graphql } from 'gatsby'
+import PlayerList from '../components/common/player-list'
+import { graphql } from 'gatsby'
 
 export default ({ data }) => (
   <Layout title="Players">
     <Container>
       <h1>Players!</h1>
-      <table>
-        <thead>
-          <th>Player</th>
-          <th>Games</th>
-          <th>Lifetime score</th>
-          <th>Magic index</th>
-        </thead>
-        <tbody>
-          {data.allAirtable.nodes.map(({ data }) => {
-            const { lifetimeScore, lifetimeGames } = getPlayerGames(data)
-            return (
-              <tr>
-                <td>
-                  <Link to={`/player/${data.Player_slug}`}>
-                    {data.Player_Name}
-                  </Link>
-                </td>
-                <td>{Math.round(lifetimeGames * 100) / 100}</td>
-                <td>{lifetimeScore}</td>
-                <td>
-                  {Math.round((lifetimeScore / lifetimeGames) * 1000) / 1000}{' '}
-                </td>
-              </tr>
-            )
-          })}
-        </tbody>
-      </table>
+      <PlayerList players={data.allAirtable.nodes} />
     </Container>
   </Layout>
 )
