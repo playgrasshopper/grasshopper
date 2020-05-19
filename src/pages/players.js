ba613885481e27439d72d874e795d5f635a77a96
Kevin Miller <keveemiller@gmail.com>
2020-05-18 20:50:21 -0400

    chore: Add magic index

--- src/pages/players.js
+++ src/pages/players.js
@@ -1,19 +1,40 @@
 import React from 'react'
 import { Container } from '../components/common/container'
 import Layout from '../components/layout'
+import getPlayerGames from '../utilities/player-games'
 import { Link, graphql } from 'gatsby'
 
 export default ({ data }) => (
   <Layout title="Players">
     <Container>
       <h1>Players!</h1>
-      <ul>
-        {data.allAirtable.nodes.map(({ data }) => (
-          <li>
-            <Link to={`/player/${data.Player_slug}`}>{data.Player_Name}</Link>
-          </li>
-        ))}
-      </ul>
+      <table>
+        <thead>
+          <th>Player</th>
+          <th>Games</th>
+          <th>Lifetime score</th>
+          <th>Magic index</th>
+        </thead>
+        <tbody>
+          {data.allAirtable.nodes.map(({ data }) => {
+            const { lifetimeScore, lifetimeGames } = getPlayerGames(data)
+            return (
+              <tr>
+                <td>
+                  <Link to={`/player/${data.Player_slug}`}>
+                    {data.Player_Name}
+                  </Link>
+                </td>
+                <td>{Math.round(lifetimeGames * 100) / 100}</td>
+                <td>{lifetimeScore}</td>
+                <td>
+                  {Math.round((lifetimeScore / lifetimeGames) * 1000) / 1000}{' '}
+                </td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
     </Container>
   </Layout>
 )
@@ -31,6 +52,42 @@ export const query = graphql`
             }
           }
           Nickname
+          Scores {
+            data {
+              Score
+              Game {
+                recordId
+                data {
+                  Date
+                  Number_of_hops
+                  Game_League {
+                    data {
+                      Name
+                      League_slug
+                    }
+                  }
+                }
+              }
+            }
+          }
+          Hops {
+            data {
+              Score
+              Game {
+                recordId
+                data {
+                  Date
+                  Number_of_hops
+                  Game_League {
+                    data {
+                      Name
+                      League_slug
+                    }
+                  }
+                }
+              }
+            }
+          }
         }
       }
     }
