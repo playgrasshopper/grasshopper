3f3186433a62c3c8565696ffcc9dfb6d1d3be8fe
Kevin Miller <keveemiller@gmail.com>
2020-05-18 20:42:50 -0400

    chore: Moved games to utilities

--- src/templates/player.js
+++ src/templates/player.js
@@ -2,43 +2,12 @@ import React from 'react'
 import { Link, graphql } from 'gatsby'
 import Layout from '../components/layout'
 import { Container } from '../components/common/container'
+import getPlayerGames from '../utilities/player-games'
 
 export default ({ data }) => {
   const { allAirtable } = data
   const player = allAirtable.nodes[0].data
-  let lifetimeScore = 0
-  let lifetimeGames = 0
-  const games = {}
-  if (player.Scores) {
-    player.Scores.forEach(score => {
-      if (score.data.Game) {
-        lifetimeScore += score.data.Score
-        lifetimeGames += score.data.Game[0].data.Number_of_hops / 3
-        games[score.data.Game[0].recordId] = {
-          recordId: score.data.Game[0].recordId,
-          ...score.data.Game[0].data,
-          score: score.data.Score,
-        }
-      }
-    })
-  }
-  if (player.Hops) {
-    player.Hops.forEach(hop => {
-      if (hop.data.Game) {
-        lifetimeScore += hop.data.Score
-        lifetimeGames += hop.data.Game[0].data.Number_of_hops / 3
-        if (typeof games[hop.data.Game[0].recordId] === 'undefined') {
-          games[hop.data.Game[0].recordId] = {
-            recordId: hop.data.Game[0].recordId,
-            ...hop.data.Game[0].data,
-            score: hop.data.Score,
-          }
-        } else {
-          games[hop.data.Game[0].recordId].score += hop.data.Score
-        }
-      }
-    })
-  }
+  const { games, lifetimeScore, lifetimeGames } = getPlayerGames(player)
   const gameList = Object.values(games).sort((a, b) => {
     return a.Date > b.Date ? -1 : 1
   })
