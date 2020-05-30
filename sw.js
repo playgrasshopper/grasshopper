fdd4507fc1a785c272cade78cc8e85d7ebc1b140
Kevin Miller <keveemiller@gmail.com>
2020-05-29 22:51:01 -0400

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-8aa94e255e2c86b2a5c2.js"
+    "url": "webpack-runtime-38717471f160692e5c91.js"
   },
   {
     "url": "framework-8770a85f31e633ae9ae0.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "2a717ecb1f0f91570b531a9a82263e68"
+    "revision": "7314c16c4228a5c75273516f3975bfc6"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-ac91495a5202f90f8ab8.js"
