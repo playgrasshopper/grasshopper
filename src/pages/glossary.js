3ed22db0eeee0152c8264f845c8284d57e89d122
Kevin Miller <keveemiller@gmail.com>
2020-06-12 14:33:53 -0600

    chore: Add ID to glossary

--- src/pages/glossary.js
+++ src/pages/glossary.js
@@ -1,19 +1,26 @@
 import React, { Fragment } from 'react'
 import { graphql } from 'gatsby'
+import slugify from 'slugify'
 import { Container } from '../components/common/container'
 import Layout from '../components/layout'
+import styled from '@emotion/styled'
 
+const Glossary = styled.dl`
+  dt {
+    font-weight: bold;
+  }
+`
 export default ({ data }) => (
   <Layout title="Glossary">
     <Container>
-      <dl>
+      <Glossary>
         {data.allGlossaryYaml.nodes.map(({ term, definition }) => (
           <Fragment key={term} t>
-            <dt>{term}</dt>
+            <dt id={slugify(term)}>{term}</dt>
             <dd>{definition}</dd>
           </Fragment>
         ))}
-      </dl>
+      </Glossary>
     </Container>
   </Layout>
 )
